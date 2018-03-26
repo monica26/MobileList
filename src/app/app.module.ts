3214dbe4abcde7770342bca86ea5d9f3d1e699e4
María Merino de Vega <maria.merino@soprasteria.com>
2018-03-26 12:30:57 +0200

    Initial structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PersonalInformationComponent } from './modules/personal-information/personal-information.component';
+import { PhoneListComponent } from './modules/phone-list/phone-list.component';
+import { SummaryComponent } from './modules/summary/summary.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { DataSummaryComponent } from './shared/components/data-summary/data-summary.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonalInformationComponent,
+    PhoneListComponent,
+    SummaryComponent,
+    FooterComponent,
+    DataSummaryComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/components/data-summary/data-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-summary',
+  templateUrl: './data-summary.component.html',
+  styleUrls: ['./data-summary.component.scss']
+})
+export class DataSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
