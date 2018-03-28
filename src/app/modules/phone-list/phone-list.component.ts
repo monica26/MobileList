5cc0d45e360ec3bbd17b722a6bca7a19ff6bd4cf
EMEAAD\movallejo <monica.vallejo@soprasteria.com>
2018-03-28 14:48:11 +0200

    Phone-list

--- src/app/modules/phone-list/phone-list.component.ts
+++ src/app/modules/phone-list/phone-list.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhoneListComponent implements OnInit {
 
+  phoneList = [
+    {alias: 'Pepe', number: '6262626262'},
+    {alias: 'Juan', number: '88872342'},
+    {alias: 'Ana', number: '222134222'},
+    {alias: 'Maria', number: '09877765'},
+    {alias: 'Luis', number: '34522344'},
+    {alias: 'Jose', number: '1234567'}
+ ];
+ public delete = false;
+ public itemHover;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  // funcion activada por mouseover al pasar por encima del elemento
+
+  mouseOver(element) {
+    console.log('mouseOver -> ' + element);
+    this.delete = true;
+    this.itemHover = element;
+  }
+
+  mouseOut(element) {
+    console.log('mouseOut -> ' + element);
+    this.delete = false;
+    this.itemHover = -1;
+  }
+
 
 }
