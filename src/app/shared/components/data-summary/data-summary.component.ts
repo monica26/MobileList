cca1b422be454669cc122b46822740ad16882c37
María Merino de Vega <maria.merino@soprasteria.com>
2018-04-03 15:59:28 +0200

    view summary

--- src/app/shared/components/data-summary/data-summary.component.ts
+++ src/app/shared/components/data-summary/data-summary.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ListSummary } from './../../models/listSummary.model';
 
 @Component({
   selector: 'app-data-summary',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-summary.component.scss']
 })
 export class DataSummaryComponent implements OnInit {
+
+  @Input() list: Array<ListSummary>;
+  @Input() sectionTitle: string;
 
   constructor() { }
 
